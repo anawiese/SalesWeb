4d874b1e05a7c80fb549ace641cde998a3dce83c
anawiese <ana.wieset@gmail.com>
2019-10-11 17:31:35 -0300

    First controller and Razor pages tests

---
+++ SalesWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SalesWeb.Models;
+
+namespace SalesWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Message"] = "Privacy policy.";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web MVC from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
